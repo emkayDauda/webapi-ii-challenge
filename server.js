ea3e4b8cf4a5a9a7a5ef9321c252c351e62a1826
Maaruf Dauda <mkdauda.work@gmail.com>
2019-10-29 17:08:34 +0100

    :sparkles: Use cors

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = require('./routers/postRoutes')
-
+const cors =  require('cors')
 const server = express();
 
+server.use(cors())
 server.use(express.json());
 
 server.use('/api/posts', router)
